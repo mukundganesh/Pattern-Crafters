5b5358b8ce608d2492c578faf4a65e9446de13fc
mukundganesh <112990679+mukundganesh@users.noreply.github.com>
2023-04-21 12:45:25 -0400

    Eighth Assignment Commit

---
+++ HW08/src/edu/umb/cs680/hw08/fs/FSElement.java
@@ -0,0 +1,39 @@
+package edu.umb.cs680.hw08.fs;
+
+import edu.umb.cs680.hw08.fs.util.FSVisitor;
+
+import java.time.LocalDateTime;
+
+public abstract class FSElement {
+    private String name;
+    private Directory parent;
+    private Integer size;
+    private LocalDateTime creationTime;
+
+    public FSElement(Directory parent,String name,Integer size,LocalDateTime creationTime){
+        this.name=name;
+        this.size=size;
+        this.parent=parent;
+        this.creationTime=creationTime;
+    }
+
+    public Directory getParent() {
+        return this.parent;
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    public String getname(){
+        return this.name;
+    }
+
+    public abstract boolean isDirectory();
+    public abstract boolean isFile();
+
+    public void accept(FSVisitor v){
+        return;
+    };
+
+}
---
+++ HW08/src/edu/umb/cs680/hw08/fs/File.java
@@ -0,0 +1,30 @@
+package edu.umb.cs680.hw08.fs;
+
+import edu.umb.cs680.hw08.fs.util.FSVisitor;
+
+import java.time.LocalDateTime;
+
+public class File extends FSElement{
+
+
+    public File(Directory parent, String name, Integer size, LocalDateTime creationTime) {
+        super(parent, name, size, creationTime);
+
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return false;
+    }
+
+    @Override
+    public boolean isFile() {
+        return true;
+    }
+
+    @Override
+    public void accept(FSVisitor v) {
+        v.visit(this);
+
+    }
+}
