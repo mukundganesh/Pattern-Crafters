f26403bf037eeb956a4dd724508c6a92a9f2bfbf
mukundganesh <112990679+mukundganesh@users.noreply.github.com>
2023-04-24 23:20:03 -0400

    Add files via upload

---
+++ HW08/test/src/edu/umb/cs680/hw08/fs/util/CountingVisitorTest.java
@@ -0,0 +1,43 @@
+package edu.umb.cs680.hw08.fs.util;
+
+import edu.umb.cs680.hw08.fs.FileSystem;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountingVisitorTest {
+
+    protected static FileSystem f;
+
+    @BeforeAll
+    public static void createfsT(){
+        f= testfixtures.createfs();
+    }
+
+    @Test
+    void getcountlinkinfs(){
+        CountingVisitor visitor=new CountingVisitor();
+        f.getRootDirs().getFirst().accept(visitor);
+        assertEquals(1,visitor.getLinkNum());
+    }
+
+    @Test
+    void getcountdirnum(){
+        CountingVisitor visitor=new CountingVisitor();
+        f.getRootDirs().getFirst().accept(visitor);
+        assertEquals(10,visitor.getDirNum());
+    }
+
+    @Test
+    void getcountfilenum(){
+        CountingVisitor visitor=new CountingVisitor();
+        f.getRootDirs().getFirst().accept(visitor);
+        assertEquals(5,visitor.getFileNum());
+    }
+
+
+
+
+
+}
