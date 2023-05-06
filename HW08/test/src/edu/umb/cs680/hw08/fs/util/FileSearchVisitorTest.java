57131f35a94e69ad0bb55b8b7122306d85000b1b
mukundganesh <112990679+mukundganesh@users.noreply.github.com>
2023-05-05 22:21:53 -0400

    HW08 After Feedback

---
+++ HW08/test/src/edu/umb/cs680/hw08/fs/util/FileSearchVisitorTest.java
@@ -0,0 +1,32 @@
+package edu.umb.cs680.hw08.fs.util;
+
+import edu.umb.cs680.hw08.fs.FileSystem;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileSearchVisitorTest {
+    protected static FileSystem f;
+    private String fileName;
+    private String actual;
+
+    @BeforeAll
+    public static void createfsT(){
+        f= testfixtures.createfs();
+    }
+
+    @Test
+    public void getnooffilescount(){
+        FileSearchVisitor visitor = new FileSearchVisitor("x");
+        f.getRootDirs().getFirst().accept(visitor);
+        assertEquals(visitor.getFoundFiles().size(), 1);
+    }
+
+    @Test
+    public void getfilenametest(){
+        FileSearchVisitor visitor = new FileSearchVisitor("x");
+        f.getRootDirs().getFirst().accept(visitor);
+        assertEquals(visitor.getFoundFiles().get(0).getname(), "x");
+    }
+}
