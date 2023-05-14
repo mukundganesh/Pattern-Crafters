44dab06dc51bb376372b849ea36bd4ab6e6454df
mukundganesh <112990679+mukundganesh@users.noreply.github.com>
2023-05-14 01:32:37 -0400

    Delete HW16 directory

--- HW16/src/edu/umb/cs680/hw16/Observer.java
+++
@@ -1,7 +0,0 @@
-package edu.umb.cs680.hw16;
-import edu.umb.cs680.hw16.StockEvent;
-
-public interface Observer<StockEvent> {
-	public void update(Observable<StockEvent> sender, StockEvent event);
-
-}
