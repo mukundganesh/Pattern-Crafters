5618cdc7fc597da9cd2775dde30c5b93003f241f
mukundganesh <112990679+mukundganesh@users.noreply.github.com>
2023-05-14 01:33:05 -0400

    Delete HW15 directory

--- HW15/src/edu/umb/cs680/hw15/EmployeeRole.java
+++
@@ -1,9 +0,0 @@
-package edu.umb.cs680.hw15;
-
-public interface EmployeeRole {
-
-        public abstract double calculateSalary(double base);
-        public abstract double calculateBonus(double base);
-        public abstract double calculateHike(double base);
-
-}
